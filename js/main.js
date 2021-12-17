7f28f1731a326603ca9b5c71397bf5eec23a1649
Fayez Ahmed <fayezkaleemahmed@gmail.com>
2021-12-17 01:00:06 -0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+fetch('http://salahhour.com/index.php/api/prayer_times?country=CA&zipcode=V3A&time_format=0')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.results)
+        document.querySelector("#fajr").innerText = data.results.Fajr
+        document.querySelector("#dhuhr").innerText = data.results.Dhuhr
+        document.querySelector("#asr").innerText = data.results.Asr
+        document.querySelector("#maghrib").innerText = data.results.Maghrib
+        document.querySelector("#isha").innerText = data.results.Isha
+    })
+    .catch(error => console.log('ERROR'))
