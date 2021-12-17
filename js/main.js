5e823bf087bfbf874ceaca4813a810dde064f228
Fayez Ahmed <fayezkaleemahmed@gmail.com>
2021-12-17 01:06:15 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-fetch('http://salahhour.com/index.php/api/prayer_times?country=CA&zipcode=V3A&time_format=0')
+fetch('https://salahhour.com/index.php/api/prayer_times?country=CA&zipcode=V3A&time_format=0')
     .then(res => res.json())
     .then(data => {
         console.log(data.results)
